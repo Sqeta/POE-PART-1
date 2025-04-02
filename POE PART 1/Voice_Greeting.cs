c375b72e6f0a21176b6d9629b48d4a125c09d800
Karabo Mohapi <karabomohapi17@outlook.com>
2025-04-02 23:05:03 +0200

    i created a class for the voice greeting message

--- POE PART 1/Voice_Greeting.cs
+++ POE PART 1/Voice_Greeting.cs
@@ -1,9 +1,45 @@
-﻿namespace POE_PART_1
+﻿using System.IO;
+using System.Media;
+using System;
+
+namespace POE_PART_1
 {
-    internal class Voice_Greeting
+    public class Voice_Greeting
     {
+        // Constructor
         public Voice_Greeting()
         {
+
+            //getting the app full location
+            string full_location = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Remove "bin\\Debug\\" from the path to get back to the main project directory
+            string new_path = full_location.Replace("bin\\Debug\\", "");
+
+            //combine the paths 
+
+            //try and catch to play the audio
+            try
+            {
+                // Get the full directory where the application is running
+                string full_path = Path.Combine(new_path, "voice greeting.wav");
+
+                // Create a SoundPlayer object and load the audio file
+                using (SoundPlayer theplayer = new SoundPlayer(full_path))
+                {
+                    // Play the audio file synchronously (program waits until the sound finishes playing)
+                    theplayer.PlaySync();
+                }
+
+            }
+            // If the file is missing or another error occurs, handle it
+            catch (Exception error)
+            {
+                // Display the error message in the console
+                Console.WriteLine(error.Message);
+            }
+
         }
+
     }
 }
